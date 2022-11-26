08e8a4e2a300f708bf00f498b4427bdef35a8415
mxswat <massimocacciapaglia.13@gmail.com>
2022-11-26 01:19:04 +0000

    works mostly

---
+++ Contents/mods/Everythingburneddown/media/lua/server/Everythingburneddown.server.lua
@@ -0,0 +1,60 @@
+local EBD = {
+    RBBurntDef = nil
+}
+
+function EBD:getBurnedDownStory()
+    if self.RBBurntDef ~= nil then
+        -- print('cached RBBurntDef')
+        return self.RBBurntDef
+    end
+
+    for i = 0, getWorld():getRandomizedBuildingList():size() - 1 do
+        local rb = getWorld():getRandomizedBuildingList():get(i);
+        if rb and rb:getName() == "Burnt" then
+            self.RBBurntDef = rb
+            -- print('rb:getName(): ', rb:getName())
+        end
+    end
+end
+
+function EBD:burnItAllDown(square)
+    if not square then
+        return
+    end
+
+    local x = square:getX()
+    local y = square:getY()
+
+
+    if isClient() then
+        return
+    end
+
+    local building = square:getBuilding()
+    if not building then
+        return
+    end
+
+    -- randomizeBuilding(BuildingDef var1) inside RBBurnt, will set building to all explored
+    if building:isAllExplored() then
+        return
+    end
+
+    local buildingDef = building:getDef()
+    if not buildingDef:isFullyStreamedIn() then
+        return
+    end
+
+    print('burn down building in square [x: ' .. x .. ', y:' .. y .. ']')
+
+    self.RBBurntDef:randomizeBuilding(buildingDef);
+end
+
+function EBD:LoadGridsquare(square)
+    self:getBurnedDownStory()
+    self:burnItAllDown(square)
+end
+
+Events.LoadGridsquare.Add(function(square)
+    EBD:LoadGridsquare(square)
+end);
